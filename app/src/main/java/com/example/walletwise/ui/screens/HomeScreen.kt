0fc178250ddc910ec128eb43c1b8536164d5b6c5
Boris <borisnandez93@gmail.com>
2024-11-04 21:42:12 -0600

    imagenes placeholders en HS

--- app/src/main/java/com/example/walletwise/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/walletwise/ui/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.walletwise.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -14,7 +15,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import coil.compose.rememberAsyncImagePainter
 import com.example.walletwise.R
 import com.example.walletwise.ui.Header
 import com.example.walletwise.ui.theme.WalletWiseTheme
@@ -23,15 +23,16 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun HomeScreen(navController: NavHostController, modifier: Modifier = Modifier) {
     Scaffold(
-        topBar = { Header(navController)}  // Mantiene el Header en la parte superior de la pantalla
+        topBar = { Header(navController) }  // Mantiene el Header en la parte superior de la pantalla
     ) { padding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)  // Asegura que el contenido no esté detrás del Header
                 .padding(horizontal = 16.dp)
         ) {
-            ContentSection()
+            item { ContentSection() } // Usa LazyColumn para poder scrollear el contenido
+            item { Spacer(modifier = Modifier.height(100.dp)) } // Espacio adicional al final
         }
     }
 }
@@ -56,7 +57,7 @@ fun ContentSection() {
             style = MaterialTheme.typography.titleLarge
         )
         Text(
-            text = "Para editar este texto, selecciona el cuadro de texto y haz doble clic...",
+            text = "¿Listo para empezar a manejar tu dinero? Abrir una cuenta bancaria es el primer paso para organizar tus finanzas. Aprende sobre los tipos de cuentas, los requisitos y cómo elegir la opción que mejor se adapta a tus necesidades.",
             style = MaterialTheme.typography.bodyMedium,
             color = Color.Gray,
             modifier = Modifier.padding(top = 8.dp)
@@ -90,11 +91,82 @@ fun ContentSection() {
             style = MaterialTheme.typography.titleLarge
         )
         Text(
-            text = "Para editar este texto, selecciona el cuadro de texto y haz doble clic...",
+            text = "¿Necesitas enviar dinero a un amigo o hacer un pago? Aquí aprenderás cómo transferir dinero de forma rápida y segura desde tu cuenta bancaria. Descubre los diferentes métodos de transferencia, sus costos y cómo evitar errores comunes.",
             style = MaterialTheme.typography.bodyMedium,
             color = Color.Gray,
             modifier = Modifier.padding(top = 8.dp)
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.banner3),
+            contentDescription = "Imagen desde URL",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "¿Qué es el Crédito y cómo Funciona?",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "Un módulo que explique qué es el crédito, cómo funciona y por qué es importante manejarlo de manera responsable. Ideal para enseñar sobre tarjetas de crédito y puntajes de crédito.",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Gray,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.banner4),
+            contentDescription = "Imagen desde URL",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Gestión de Gastos Diarios",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "Un módulo práctico para aprender a monitorear y gestionar los gastos diarios, enseñando sobre presupuestos y categorías de gastos.",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Gray,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.banner5),
+            contentDescription = "Imagen desde URL",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "La Importancia de un Fondo de Emergencia",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "Módulo que enfatice la importancia de tener un fondo de emergencia y cómo calcular la cantidad ideal.",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Gray,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        Spacer(modifier = Modifier.height(100.dp))
     }
 }
 
